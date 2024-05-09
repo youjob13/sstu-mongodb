b8f72216aca1b7ada7a371f292e1d1ea99120b2f
Danil Rodionov <Danil_Rodionov@epam.com>
2024-05-09 20:06:21 +0400

    f

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ class App {
     server.listen(3000, async () => {
       console.log("Server is running on port 3000");
       await connectToMongoDB();
-      const services = await getServices();
-      console.log(services);
     });
   }
 
